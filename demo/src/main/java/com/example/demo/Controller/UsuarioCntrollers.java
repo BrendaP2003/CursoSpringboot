870f2358492a76a0941d46703f2d2fba7660d7c6
Brenda <brenda15padilla@gmail.com>
2025-02-21 23:31:12 -0700

    creacion del controlador usuario

--- demo/src/main/java/com/example/demo/Controller/UsuarioCntrollers.java
+++ demo/src/main/java/com/example/demo/Controller/UsuarioCntrollers.java
@@ -1,15 +1,14 @@
 package com.example.demo.Controller;
 
 
+import com.example.demo.Dto.Request.UsuarioRequest;
 import com.example.demo.Dto.Response.UsuarioResponse;
 import com.example.demo.Servicio.UsuarioService;
 //import org.apache.el.stream.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Optional;
 import java.util.ArrayList;
 
@@ -29,7 +28,7 @@ public class UsuarioCntrollers {
     }
     //obtener usuario por id
 
-    @GetMapping("/usuarios/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<UsuarioResponse> obtenerUsuarioPorId(@PathVariable Long id) {
         Optional<UsuarioResponse> usuarioResponse = usuarioservice.ObtenerPorId(id);
 
@@ -38,5 +37,29 @@ public class UsuarioCntrollers {
                 .map(ResponseEntity::ok) // Mapea el valor a ResponseEntity.ok()
                 .orElseGet(() -> ResponseEntity.notFound().build()); // Si no está presente, devuelve 404
     }
+    //Guardar Usuario
+     @PostMapping
+    public ResponseEntity<UsuarioResponse> GuardarUsuario(@RequestBody UsuarioRequest usuarioRequest){
+        UsuarioResponse UsuarioGuardado = usuarioservice.GuardarUsuarios(usuarioRequest);
+
+        return ResponseEntity.ok(UsuarioGuardado);
+    }
+
+    @DeleteMapping("/{id}")
+    //eliminar Usuario
+    public ResponseEntity <Void> EliminarUsuario (@PathVariable Long id){
+        boolean usuarioeliminado = usuarioservice.EliminarUsuarios(id);
+
+        if (usuarioeliminado){
+
+            return ResponseEntity.noContent().build();
+        }else{
+
+            return  ResponseEntity.notFound().build();
+        }
+
+
+    }
+
 
 }
